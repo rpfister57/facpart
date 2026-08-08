03d4c1dcccf3a34750791d6109dc6abaa751ba11
RPfister <pfister@RPfisters-iMac.fritz.box>
2026-08-08 20:33:42 +0200

    gutt91 data modified

--- R/data.R
+++ R/data.R
@@ -10,9 +10,7 @@
 #'   \item{gfacets}{A factor with 4 levels: analytical, complex, achievement1, achievement2}
 #'   \item{D1, D2}{Coordinates from a 2-dimensional MDS}
 #' }
-#' 
 #' @source smacof package, data(Guttman1965)
-#' 
 #' @examples
 #' plot(guttman65mds[ , 2:3], type = "n", asp = 1)
 #' text(guttman65mds[ , 2:3], labels = guttman65mds$gfacets, cex = 0.6)
@@ -25,35 +23,36 @@
 
 #' Guttman 1991 Intellligence Data
 #' 
-#' Data are based on the smacof package. It contains the correlations
-#' among 12 intelligence items, and the assignment to two facets:
-#' Material and Modality.
+#' Data are based on the smacof package and on the original article by 
+#' Guttman and Levy (1991). The 12 test items are actually 12 subtests 
+#' from the WISC-R, and the sample are 2200 U.S. children 
+#' aged 6.5 to 16.5 years (see Guttman & Levy, 1991).
 #' 
 #' @format
-#' A list with two elements: gutt91_cor, gutt91_var
+#' A list with three components: gutt91_cor, gutt91_dis, gutt91_df:
 #' \describe{
 #'   \item{gutt91_cor}{A correlation matrix of 12 intelligence items}
-#'   \item{gutt91_var}{A data frame with 12 observations and 3 variables:
-#'   items = short name of the item, Material = Facet 1 (Oral, Manual, Paper),
-#'   Modality = Facet 2 (verbal, numerical, figural)}
+#'   \item{gutt91_dis}{A distance matrix derived from the correlation matrix}
+#'   \item{gutt91_df}{A data frame with 12 rows and 9 variables:}
+#'   \item{items}{The labels for the tests}
+#'   \item{Material}{The assignment of the Material facet (oral, manual, paper)}
+#'   \item{Modality}{The assignment of the Modality facet (verbal, numerical, figural)}
+#'   \item{Rule}{The assignment of the Rule Task facet (infer, applFact, applSTM)}
+#'   \item{X1, X2}{Coordinates from a 2-dimensional ordinal MDS}
+#'   \item{D1, D2, D3}{Coordinates from a 3-dimensional ordinal MDS}
 #' }
-#' 
 #' @source smacof package, data(Guttman1991)
-#' 
 #' @references
 #' Guttman, L. & Levy, S. (1991). Two structural laws for intelligence tests. Intelligence, 15, 79-103.
-#' 
 #' @examples
 #' data(gutt91)
-#' Kor <- gutt91$gutt91_cor
-#' Facets <- gutt91$gutt91_var
-#' # convert correlations to distances
-#' Kor_D <- smacof::sim2diss(Kor, method = "corr", to.dist = TRUE)
+#' Kor_D <- gutt91$gutt91_dis
+#' Facets <- gutt91$gutt91_df
 #' # MDS
 #' gutt91_mds <- smacof::mds(Kor_D, type = "ordinal")
-#' plot(gutt91_mds, main = "Guttman 1991 Intelligence")
+#' plot(gutt91_mds, main = "Guttman Levy 1991 Intelligence")
 #' # Angular partition of Modality
-#' angularPartition(crd = gutt91_mds$conf, group = Facets$Modality)
+#' angularPartition(crd = gutt91_mds$conf, group = Facets$Modality, add = FALSE)
 #' 
 "gutt91"
 
